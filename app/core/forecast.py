acb35cfd1ab7a34a3405398167bb5642b68d324c
Heorhii <geochernykh@gmail.com>
2026-03-06 16:28:15 +0200

    not final

--- app/core/forecast.py
+++ app/core/forecast.py
@@ -19,15 +19,18 @@ def get_forecast(location, start_date, end_date, unit_group="metric"):
     request_url = f"{BASE_URL}/{location}/{start_date}/{end_date}?unitGroup={unit_group}&key={WEATHER_API_KEY}&include=hours&elements={elements}&lang=en"
 
     response = requests.get(request_url)
+    print(response.json())
 
     if response.status_code == requests.codes.ok:
-        return json.loads(response.text)
+        return json.loads(format_forecast(response, start_date, end_date))
     else:
         raise InvalidUsage(response.text, status_code=response.status_code)
 
 
-def format_forecast(raw_forecast, start_date, end_date, curr_datetime):
+def format_forecast(raw_forecast, start_date, end_date):
     """Transform raw forecast data into hourly forecast dict keyed by datetime."""
+    curr_datetime = dt.datetime.now()
+
     days = raw_forecast.get("days")
     hours = []
     for day in days:
@@ -48,3 +51,5 @@ def format_forecast(raw_forecast, start_date, end_date, curr_datetime):
     return forecast
 
 
+if __name__ == "__main__":
+    print(get_forecast(location="Kyiv,Ukraine", start_date="2026-03-06", end_date="2026-03-06"))
