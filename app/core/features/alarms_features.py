b902c407ab8128a5e2a9b99b6b81aa2e537e0068
Heorhii <geochernykh@gmail.com>
2026-03-30 00:35:02 +0300

    Project structure changed
    Core folder now have subfolders for scripts that scrape data and scripts that create features from raw data.

---
+++ app/core/features/alarms_features.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def create_features_alarms(df) -> pd.DataFrame:
+    pass
---
+++ app/core/features/telegram_features.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+
+def create_features_telegram(df) -> pd.DataFrame:
+    pass
